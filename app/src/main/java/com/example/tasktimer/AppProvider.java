a13b6286ebf392dca6157a1e6ad58b6385841bc5
Liam <urehelonn@gmail.com>
2020-02-22 01:02:16 -0500

    init commit with content upload

---
+++ app/src/main/java/com/example/tasktimer/AppProvider.java
@@ -0,0 +1,133 @@
+package com.example.tasktimer;
+
+// Provider, for database { @link AppDatabase }
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class AppProvider extends ContentProvider {
+
+    private static final String TAG = "AppProvider";
+
+    private AppDatabase dbHelper;
+
+    public static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static final String CONTENT_AUTHORITY = "com.example.tasktimer.provider";
+    public static final Uri CONTENT_AUTHORITY_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final int TASKS = 100;
+    public static final int TASKS_ID = 101;
+
+    public static final int TIMINGS = 200;
+    public static final int TIMINGS_ID = 201;
+
+//    private static final int TASK_TIMES = 300;
+//    private static final int TASK_TIMES_ID = 301;
+
+    public static final int TASK_DURATIONS = 400;
+    public static final int TASK_DURATIONS_ID = 401;
+
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        //eg. content://com.example.tasktimer.provider.Tasks
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME, TASKS);
+        //eg. content://com.example.tasktimer.provider.Tasks/8
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME + "/#", TASKS_ID);
+
+//        //eg. content://com.example.tasktimer.provider.Timing
+//        matcher.addURI(CONTENT_AUTHORITY, TimingContract.TABLE_NAME, TIMINGS);
+//        //eg. content://com.example.tasktimer.provider.Timing/8
+//        matcher.addURI(CONTENT_AUTHORITY, TimingContract.TABLE_NAME + "/#", TIMINGS_ID);
+//
+//        //eg. content://com.example.tasktimer.provider.Timing
+//        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME, TASK_DURATIONS);
+//        //eg. content://com.example.tasktimer.provider.Timing/8
+//        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME + "/#", TASK_DURATIONS_ID);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = AppDatabase.getInstance(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        Log.d(TAG, "query: called with URI " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "query: match is " + match);
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        switch (match) {
+            case TASKS:
+                queryBuilder.setTables(TasksContract.TABLE_NAME);
+                break;
+            case TASKS_ID:
+                queryBuilder.setTables(TasksContract.TABLE_NAME);
+                long taskId = TasksContract.getTaskId(uri);
+                queryBuilder.appendWhere(TasksContract.Columns._ID + " = " + taskId);
+                break;
+
+//            case TIMINGS:
+//                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+//                break;
+//            case TIMINGS_ID:
+//                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+//                long timingId = TimingsContract.getTimingId(uri);
+//                queryBuilder.appendWhere(TimingsContract.Columns._ID + " = " + timingId);
+//                break;
+//
+//            case TASK_DURATIONS:
+//                queryBuilder.setTables(DuratioinsContract.TABLE_NAME);
+//                break;
+//            case TASK_DURATIONS_ID:
+//                queryBuilder.setTables(DuratioinsContract.TABLE_NAME);
+//                long durationId = DuratioinsContract.getTimingId(uri);
+//                queryBuilder.appendWhere(DuratioinsContract.Columns._ID + " = " + durationId);
+//                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/tasktimer/MainActivityViewModel.java
@@ -0,0 +1,7 @@
+package com.example.tasktimer;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
